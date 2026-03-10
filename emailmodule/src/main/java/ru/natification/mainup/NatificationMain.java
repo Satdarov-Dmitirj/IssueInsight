ccc14cd8f77956498a916cacdb2654086a7f43bb
Satdarov-Dmitirj <satdarovdmitryy@ya.ru>
2026-03-10 15:03:49 +0300

    refactor: split project into multi-module structure
    - Created parent pom.xml with two modules: ticket-service and email-module
    - Moved all existing classes to ticket-service module
    - Added empty email-module for future email notification functionality

---
+++ emailmodule/src/main/java/ru/natification/mainup/NatificationMain.java
@@ -0,0 +1,11 @@
+package ru.natification.mainup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatificationMain {
+    public static void main(String[] args) {
+        SpringApplication.run(NatificationMain.class, args);
+    }
+}
---
+++ emailmodule/src/main/java/ru/natification/mainup/configuration/ConfigurationKafka.java
@@ -0,0 +1,46 @@
+package ru.natification.mainup.configuration;
+
+import com.zaxxer.hikari.util.ClockSource;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.hibernate.engine.jdbc.connections.internal.ConnectionCreatorFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import tools.jackson.databind.deser.jdk.StringDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+@EnableKafka
+@Configuration
+public class ConfigurationKafka {
+
+    String bootstrap;
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "groupSeccion");
+       // props.put(ConsumerConfig.)
+
+        return new DefaultKafkaConsumerFactory<>(props);
+
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> KafkaLisener(){
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+}
+
